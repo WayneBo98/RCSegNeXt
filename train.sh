e7ce69684dcef64a8cceec826c973298fdec88c9
WayneBo98 <82822433+WayneBo98@users.noreply.github.com>
2025-03-08 15:41:24 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIBLE_DEVICES=1
+nnUNetv2_train 662 3d_fullres 0 -tr nnUNetTrainerRCSegNeXt
+nnUNetv2_train 662 3d_fullres 1 -tr nnUNetTrainerRCSegNeXt
+nnUNetv2_train 662 3d_fullres 2 -tr nnUNetTrainerRCSegNeXt
+nnUNetv2_train 662 3d_fullres 3 -tr nnUNetTrainerRCSegNeXt
+nnUNetv2_train 662 3d_fullres 4 -tr nnUNetTrainerRCSegNeXt
